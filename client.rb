146376c9fce1cc2cc3fe76122ebd2358d10e7d8a
Ulvrun <ulvrunarium@gmail.com>
2016-05-06 01:05:02 +0300

    Measure Body Sizes Method

--- client.rb
+++ client.rb
@@ -2,14 +2,16 @@ require_relative 'person'
 
 class Client < Person
 
-  attr_accessor :weight
+  attr_reader :first_name, :last_name, :height, :weight
+  attr_accessor :bust, :waist, :hip
 
-  def initialize(first_name, last_name, age, gender, weight)
+  def initialize(first_name, last_name, age, gender, height, weight)
 
     @first_name = first_name
     @last_name = last_name
     @age = age
     @gender = gender
+    @height = height
     @weight = weight
 
     puts "New client is created: #{first_name} #{last_name}."
@@ -17,6 +19,13 @@ class Client < Person
 
   end
 
-end
+  def measure_body_sizes(bust, waist, hip)
 
-client_zkurilo = Client.new('Zhenya', 'Kurilo', 31, :male, 71)
+    @bust = bust
+    @waist = waist
+    @hip = hip
+
+    puts "Body sizes parameters: bust = #{bust}, waist = #{waist}, hip = #{hip}"
+
+  end
+end
