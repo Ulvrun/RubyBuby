aeed28e30d63f57f43f759ceb985bec6813518a8
Ulvrun <ulvrunarium@gmail.com>
2016-05-06 00:40:24 +0300

    Calculation of Body Mass Index by Trainer

--- trainer.rb
+++ trainer.rb
@@ -1,5 +1,6 @@
 
 require_relative 'person'
+require_relative 'client'
 
 class Trainer < Person
 
@@ -19,4 +20,30 @@ class Trainer < Person
 
   end
 
+  def calc_body_mass_index(client)
+
+    puts "Calculate BMI for #{client.first_name} #{client.last_name}"
+    puts "Height #{client.height}"
+    puts "Weight #{client.weight}"
+
+    # Body Mass Index is defined as the body mass divided by the square of the body height
+    bmi = (client.weight / ((client.height * 0.01) ** 2)).round
+
+    puts "Client's BMI is #{bmi}"
+
+    case bmi
+
+      when 0...18.5
+        puts 'Under Weight'
+      when 18.5..24.9
+        puts 'Normal Weight'
+      when 25..29.9
+        puts 'Over Weight'
+      when 30..39.99
+        puts 'Obese'
+      when bmi > 40
+        puts 'Morbidly Obese'
+    end
+
+  end
 end
