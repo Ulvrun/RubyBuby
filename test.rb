7145a2bdeb839c4076214c96692c2a3135c19876
Irina Kuznetsova <mafaya@elance-odesk.com>
2016-05-06 01:10:35 +0300

    Test - comits should be reflected in profile

--- test.rb
+++ test.rb
@@ -4,7 +4,11 @@ require_relative 'client'
 
 vlad_trainer = Trainer.new('Vlad', 'Skrishevskiy', 31, :male, 0.5)
 zhenya_client = Client.new('Zhenya', 'Kurilo', 31, :male, 172, 71)
+irina_client = Client.new('Irina', 'Kuznetsova', 25, :female, 168, 57)
 
 zhenya_client.measure_body_sizes(88, 70, 93)
+
 zhenya_bmi = vlad_trainer.calc_body_mass_index(zhenya_client)
+irina_bmi = vlad_trainer.calc_body_mass_index(irina_client)
+
 vlad_trainer.prepare_diet_tips(zhenya_bmi)
